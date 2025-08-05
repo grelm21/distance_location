c0751aa23548a2dc60c4b25be9bd59ddff9c7808
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-08-05 11:07:27 +0300

    typo fixes, added report files to attachemts folder

--- app/controllers/concerns/distance_concern.rb
+++ app/controllers/concerns/distance_concern.rb
@@ -2,7 +2,7 @@ module DistanceConcern
   extend ActiveSupport::Concern
 
   def calculate_straight_distance(from, to)
-    (from.lonlat.distance(to.lonlat)/1000).round(2)
+    (from.lonlat.distance(to.name)/1000).round(2)
   rescue StandardError
     raise DistanceHandler::DistanceCalculationError.new('DistanceConcern#calculate_distance')
   end
